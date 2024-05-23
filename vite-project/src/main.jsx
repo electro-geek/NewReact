bf15c563abddfde1edec3e7360c7b6b10ddeed04
geek-electro <mritunjaypandey0789@gmail.com>
2024-05-23 11:34:57 +0530

    Something commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,7 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Something from './something.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+}
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },//tells you the properties
+    children: 'click me to visit google' 
+}
+
+const anotherElement = (
+    <a href='https://google.com' target='_blank'>Visit google</a>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-);
+    
+)
