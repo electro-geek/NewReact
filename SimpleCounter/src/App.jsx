e252c1b105cdbfc9195740a3758f7b7ab9443dc5
geek-electro <mritunjaypandey0789@gmail.com>
2024-05-24 15:26:47 +0530

    6th lecture done

--- SimpleCounter/src/App.jsx
+++ SimpleCounter/src/App.jsx
@@ -3,8 +3,25 @@ import { useState } from 'react'
 function App() {
   const [counter, setCounter] = useState(0)
 
+  //set counter accepts accepts call back function
+
   const addValue = () => {
-    useState
+    //below code will not increment the value of counter again and again
+    //react will consider this bundle together
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter -1)
   }
 
   return (
@@ -14,6 +31,8 @@ function App() {
       <button
       onClick={addValue}
       >Add Value</button>
+      <br></br>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
